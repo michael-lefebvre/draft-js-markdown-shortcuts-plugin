7b71ca98e9eb08255592ff2ae6d9013840965215
Michael Lefebvre <michael@scenedata.com>
2017-03-26 09:05:19 +0200

    Dirty fix for conflict with mention plugin
    ngs/draft-js-markdown-shortcuts-plugin/issues/15

--- src/decorators/link/linkStrategy.js
+++ src/decorators/link/linkStrategy.js
@@ -3,11 +3,13 @@ import { Entity } from 'draft-js';
 const createLinkStrategy = () => {
   const findLinkEntities = (contentBlock, callback) => {
     contentBlock.findEntityRanges((character) => {
-      const entityKey = character.getEntity();
-      return (
-        entityKey !== null &&
-        Entity.get(entityKey).getType() === 'LINK'
-      );
+      try {
+        const entityKey = character.getEntity();
+        return (
+          entityKey !== null &&
+          Entity.get(entityKey).getType() === 'LINK'
+        );
+      } catch( e ) {}
     }, callback);
   };
   return findLinkEntities;
